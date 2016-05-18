60f9c0987e53622f1f366098115e1bab7b14815d
Lukas Kasimor <lkasimor2016@room-3.local>
2016-05-18 11:02:03 -0700

    most updated

--- iOS/Koreo/Koreo/ViewController.swift
+++ iOS/Koreo/Koreo/ViewController.swift
@@ -11,12 +11,50 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var table: UITableView!
+    
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
     @IBAction func allClasses(sender: AnyObject) {
+
+        tableArray = []
+        for elective in electivesArray {
+            tableArray.append(elective)
+        }
+        for required in requiredArray {
+            tableArray.append(required)
+        }
+        table.reloadData()
+        let classes = [requiredArray, electivesArray]
+        let sortedClasses = classes.sort { $0.name < $1.name }
+        print(sortedClasses)
+
     }
     @IBAction func requiredClasses(sender: AnyObject) {
+        tableArray = []
+        tableArray = requiredArray
+        table.reloadData()
     }
-    @IBOutlet weak var electiveClasses: UIButton!
     
+    @IBAction func electiveClasses(sender: AnyObject) {
+        tableArray = []
+        tableArray = electivesArray
+        table.reloadData()
+    }
+    
+    
+    
+    let requiredArray = ["Algebra I", "Advanced Algebra", "Geometry Functions", "Intro to Lit", "Intro to Comp", "American Literature", "World Literature", "Biology", "Physics", "Chemistry", "Conceptual Physics", "European History", "World History", "United States History", "Modern American Social History", "Spanish I", "Spanish II", "Mandarin I", "Mandarin II"]
+    
+    let electivesArray = ["Jazz Band", "Concert Band", "Graphic Design", "Computer Programming", "Art I & II", "Art III & IV", "Theatre Arts", "Economics"]
+
+    var tableArray: [String] = []
+    
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,13 +77,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         if cell == nil {
             cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
         }
-        cell?.textLabel?.text = "Class Name"
+        cell?.textLabel?.text = tableArray[indexPath.row]
         
         return cell!
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return tableArray.count
     }
 
 
